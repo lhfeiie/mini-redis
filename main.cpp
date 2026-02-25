c06874da44162629e544b5bea7bf6ec6c39267d8
lihaofei <lihaofei66@gmail.com>
2026-02-25 20:30:08 +0800

    Day 4: 添加CMakeLists.txt，配置CMake构建系统

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello! Mini-Redis project starts!" << std::endl;
     std::cout << "Day 3: Learning Git." << std::endl;
+    std::cout << "Day 4: Learning CMake." << std::endl;
     return 0;
 }
